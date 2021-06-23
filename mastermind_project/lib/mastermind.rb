0b9d94a24dfced6879a9ab94d71fb1b85ea993cf
David <david.espinosa.21@gmail.com>
2021-06-23 10:41:28 -0500

    Added completed Mastermind project

---
+++ mastermind_project/lib/mastermind.rb
@@ -0,0 +1,19 @@
+require_relative "code"
+
+class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
+
+    def print_matches(code_obj)
+        p @secret_code.num_exact_matches(code_obj)
+        p @secret_code.num_near_matches(code_obj)
+    end
+
+    def ask_user_for_guess
+        puts 'Enter a code'
+        user_code_obj = Code.from_string(gets.chomp)
+        print_matches(user_code_obj)
+        @secret_code == user_code_obj
+    end
+end
