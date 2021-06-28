0b19acffb6ab55d6b85ababb800462ce2335f84c
your name hereEddy_Marshall <epmarsha2@gmail.com>
2021-06-28 16:22:18 -0400

    first swap

--- practice.rb
+++ practice.rb
@@ -1,27 +1,43 @@
 class Array
 
-    def my_each(&prc)
-        i = 0
-        while count < (self.length) - 1
-            prc.call(self[i])
-            i += 1
-        end
+  def my_each(&prc)
+    i = 0
+    while i < self.length
+      prc.call(self[i])
+      i += 1
     end
+    self
+  end
+
+  def my_select(&prc)
+    new_arr = self.my_each {|num| num } 
+    new_arr.each_with_index do |ele, i|
+      if prc.call(ele)
+        new_arr.delete_at(i)
+      end 
+    end
+    new_arr
+  end
 
 end
 
 
-return_value = [1, 2, 3].my_each do |num|
-  puts num
-end
-.my_each do |num|
-  puts num
-end
-# => 1
-     2
-     3
-     1
-     2
-     3
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+# # => 1
+# #      2
+# #      3
+# #      1
+# #      2
+# #      3
 
-p return_value  # => [1, 2, 3]
+# p return_value  # => [1, 2, 3]
+
+
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
