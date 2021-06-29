431f86583bc3bad12491976e0b1de7c16b565ea7
Shahdi Qurashi <shahdiqurashi@gmail.com>
2021-06-29 12:11:28 -0400

    First Switch

---
+++ card.rb
@@ -0,0 +1,29 @@
+class Card
+    attr_reader :value, :is_face_up
+
+    def intialize(value)
+        @is_face_up = false
+        @value = value 
+    end
+
+    def hide
+        @is_face_up = false     
+    end 
+
+    def reveal
+        @is_face_up = true 
+    end 
+
+    def ==(other_card)
+        self.value == other_card.value
+    end 
+
+    def to_s
+        if @is_face_up 
+            print @value.to_s
+        else  
+            print " " 
+        end 
+    end
+
+end     
