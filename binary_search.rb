24dc3b11aae49eb400fd63afb01eebb64c53bfc5
David <david.espinosa.21@gmail.com>
2021-06-30 16:02:31 -0500

    finished binary search!

--- binary_search.rb
+++ binary_search.rb
@@ -8,7 +8,8 @@ def bsearch(arr, target)
         end
     end
     side = nil
-
+    a = 0
+    b = 0
     mid_position = arr.length / 2
     if arr[mid_position] == target
         return mid_position
@@ -19,22 +20,22 @@ def bsearch(arr, target)
         b = bsearch(arr[0...mid_position],target) #b=0
         side = false
     end
-    if a == nil || b== nil
+    if a == nil || b == nil
         return nil
     end
     idx = 0
     if side
-        idx += b + arr.length/2 + 1
-    else 
-        idx += a
+        idx += a + arr.length/2 + 1
+    else
+        idx = b
     end
     idx
 
 end
-# p bsearch([1, 2, 3], 1) # =>0
-# p bsearch([2, 3, 4, 5], 3) # => 1
-# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 2, 3], 1) # =>0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
 p bsearch([1, 3, 4, 5, 9], 5) # => 3
-# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
