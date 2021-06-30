4502bd6c8ac9ea78713e6b14c0d3c84583da31df
David <david.espinosa.21@gmail.com>
2021-06-30 13:45:07 -0500

    exponentiation

--- exponentiation.rb
+++ exponentiation.rb
@@ -1,1 +1,22 @@
-#hello
+def exp1(base, pow)
+    return 1 if pow == 0
+    base * exp1(base, pow - 1)
+end
+
+# p exp1(2, 5)
+
+
+def exp2(base, pow)
+    return 1 if pow == 0
+    return base if pow == 1
+    if pow.even?
+        a = exp2(base, pow / 2) 
+        a * a
+    else
+        b = exp2(base, (pow - 1) / 2)
+        base * (b * b)
+    end
+end
+
+p exp2(2, 5)
+p exp2(2, 6)
