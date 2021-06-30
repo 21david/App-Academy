9e7e8c46969462e47146518f5ccb036461242881
julian <juka1031@gmail.com>
2021-06-30 16:42:56 -0400

    still binary

--- binary_search.rb
+++ binary_search.rb
@@ -1,28 +1,40 @@
+require 'byebug'
 def bsearch(arr, target)
-    if arr.length == 1 && arr.include?(target)
-        return true
+    if arr.length == 1
+        if arr[0] == target
+            return 0
+        else
+            return nil
+        end
     end
     side = nil
 
     mid_position = arr.length / 2
     if arr[mid_position] == target
-        return true
+        return mid_position
     elsif arr[mid_position] < target
         a = bsearch(arr[mid_position+1..-1],target)
         side = true
     else
-        b = bsearch(arr[0...mid_position],target) #([1],1) b= true after being recursively called
+        b = bsearch(arr[0...mid_position],target) #b=0
         side = false
     end
-    # idx = 0
-    # if side
-    #     idx += arr.length/2
-    # end
-    # idx
-    if a == true || b == true
-        return arr.index(target)
+    if a == nil || b== nil
+        return nil
     end
+    idx = 0
+    if side
+        idx += b + arr.length/2 + 1
+    else 
+        idx += a
+    end
+    idx
+
 end
-p bsearch([1, 2, 3], 1) # =>0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 2, 3], 1) # =>0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
