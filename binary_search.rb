6bea8ef0c865251c6c147ec63265a2c97471ca99
julian <juka1031@gmail.com>
2021-06-30 15:43:44 -0400

    binary part

--- binary_search.rb
+++ binary_search.rb
@@ -1,14 +1,24 @@
 def bsearch(arr, target)
     if arr.length == 1 && arr.include?(target)
         return true
-    else
+    end
+    side = nil
+
     mid_position = arr.length / 2
     if arr[mid_position] == target
         return mid_position
-    elsif arr[mid_position] > target
-        bsearch(arr[mid_position+1..-1])
+    elsif arr[mid_position] < target
+        a = bsearch(arr[mid_position+1..-1],target)
+        side = true
     else
-        bsearch(arr[0...mid_position])
+        b = bsearch(arr[0...mid_position],target) #([1],1) b= true after being recursively called
+        side = false
     end
-
+    idx = 0
+    if side
+        idx += arr.length/2
+    end
+    idx
 end
+p bsearch([1, 2, 3], 1) # =>0
+p bsearch([2, 3, 4, 5], 3) # => 1
