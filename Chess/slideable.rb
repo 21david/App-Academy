46a4ed725ef38a61079928df381610e3e437f6d6
David <david.espinosa.21@gmail.com>
2021-07-06 14:21:40 -0500

    slideable

--- Chess/slideable.rb
+++ Chess/slideable.rb
@@ -1,7 +1,17 @@
 module Slideable
 
-    HORIZONTAL_DIRS = []
-    DIAGONAL_DIRS = []
+    HORIZONTAL_DIRS = [[0,1], [0,-1], [1,0], [-1,0]]
+    DIAGONAL_DIRS = [[1,1], [1,-1], [-1,1], [-1,-1]]
 
-    
+    def moves
+        possible_moves = []
+        
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+        new_pos = []
+        new_pos << @pos[0] + dy
+        new_pos << @pos[1] + dx
+        new_pos
+    end
 end
