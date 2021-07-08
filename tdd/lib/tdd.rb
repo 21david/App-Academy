0243423b9b883f3e92a1cf3c30e3d53f7e54ae8a
Sydney <sydneyparsonsyb@gmail.com>
2021-07-08 15:11:06 -0400

    two_sum

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -7,3 +7,21 @@ def my_uniq(arr)
     end
     uniques
 end
+
+
+class Array
+
+    def two_sum
+        new_arr= []
+
+        self.each_with_index do |ele, idx|
+            self.each_with_index do |ele2, idx2|
+                if ele + ele2 == 0 && idx < idx2
+                    new_arr << [idx, idx2]
+                end
+            end
+        end
+        new_arr
+    end
+
+end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -15,16 +15,18 @@ end
 
 describe Array do 
     describe '#two_sum' do
+        arr = [-1, 0, 2, -2, 1]
         it 'find all pairs of elements that sum to 0' do
-
+            expect(arr.two_sum).to include([0, 4], [2, 3])
         end
 
         it 'returns an empty array if there are no pairs that sum to 0' do
-
+            expect([1, 2, 3, 4].two_sum).to eq([])
+            expect([].two_sum).to eq([])
         end
 
         it 'returns a 2 dimentional array with sorted pairs' do
-
+            expect(arr.two_sum).to eq([[0, 4], [2, 3]])
         end
     end
 end
