faea06e6ff4db51d62cea9204450531c475e78b9
David <david.espinosa.21@gmail.com>
2021-07-09 14:38:24 -0500

    two_sum

---
+++ two_sum.rb
@@ -0,0 +1,31 @@
+def bad_two_sum?(arr, target_sum)  # O(N^2)
+
+    (0...arr.length-1).each do |i|
+        (i+1...arr.length).each do |j|
+            return true if arr[i] + arr[j] == target_sum
+        end
+    end
+
+    false
+end
+
+arr = [0, 1, 5, 7]
+p bad_two_sum?(arr, 6) # => should be true
+p bad_two_sum?(arr, 10) # => should be false
+
+
+def okay_two_sum?(arr, target_sum)
+    sorted = arr.sort
+
+    arr2 = []
+    sorted.each do |num|
+        arr2 << target_sum - num
+    end
+
+    arr.each do |num|
+        result = sorted.bsearch { |x| x == num }
+        return true if result
+    end
+
+    false
+end
