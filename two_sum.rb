fdfca0c71cdf03f0cd2e790d3ea4e283c1b2d470
Wajahat Shoukat <wajahatshoukat@gmail.com>
2021-07-09 16:01:58 -0400

    okay_two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -14,18 +14,21 @@ p bad_two_sum?(arr, 6) # => should be true
 p bad_two_sum?(arr, 10) # => should be false
 
 
-def okay_two_sum?(arr, target_sum)
+def okay_two_sum?(arr, target_sum) 
     sorted = arr.sort
 
-    arr2 = []
+    arr2 = [6, 5, 1, -1]
     sorted.each do |num|
         arr2 << target_sum - num
     end
 
-    arr.each do |num|
+    arr2.each do |num|
         result = sorted.bsearch { |x| x == num }
         return true if result
     end
 
     false
 end
+
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
