9d6d4a954d0634ebf7fa15a428068b9519a256f7
Jonathan Wong <jonathan.wong8@baruchmail.cuny.edu>
2021-07-12 11:51:54 -0400

    commit

--- my_hash_map.rb
+++ my_hash_map.rb
@@ -21,11 +21,11 @@ end
 
 class IntSet
     def initialize
-        @array = Array.new(20) { Array.new }
+        @store = Array.new(20) { Array.new }
     end
 
     def [](num)
-        @array[num]
+        @store[num]
     end
 
     def insert(num)
@@ -38,5 +38,40 @@ class IntSet
 
     def include?(num)
         self[num % 20].include?(num)
+    end
+end
+
+class ResizingIntSet
+     def initialize
+        @store = Array.new(5) { Array.new }
+        @size = 0
+    end
+
+    def insert(n)
+        @size += 1 
+        resize if @size >= @store.length
+        @store[n % @store.length] << n 
+    end
+
+    def remove(n)
+        @size -= 1
+        @store[n % @store.length].delete(n)
+    end
+
+    def include?(n)
+        @store[n % @store.length].include?(n)
+    end
+    
+    private
+
+    def resize
+        new_size = @store.length + 10
+        new_store = Array.new(new_size) { Array.new }
+        @store.each do |arr|
+            arr.each do |ele|
+                new_store[ele % new_size] << ele
+            end
+        end
+        @store = new_store
     end
 end
