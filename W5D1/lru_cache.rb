4a2391c1e7a2c3e71a1ddec936faa3bc9fa0c24b
David <david.espinosa.21@gmail.com>
2021-07-11 19:41:59 -0500

    Finish LRU cache homework (W5D1)

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -10,44 +10,63 @@ class LRUCache
     end
 
     def count
-      # returns number of elements currently in cache
-      @arr.length
+        # returns number of elements currently in cache
+        @arr.length
     end
 
     def add(added_ele)
-      # adds element to cache according to LRU principle
+        # adds element to cache according to LRU principle
 
-      # we must first search the array to see if the item is already there
-      found = false
-      @arr.each_with_index do |ele, i|
-        if ele == added_ele
-            # this code 'bumps' the element to the top (to the most recently used slot)
-            shift_elements(i)
-            @arr[-1] = added_ele 
-            found = true
+        # we must first search the array to see if the item is already there
+        found = false
+        @arr.each_with_index do |ele, i|
+            if ele == added_ele
+                # this code 'bumps' the element to the top (to the most recently used slot)
+                shift_elements(i)
+                @arr[-1] = added_ele 
+                found = true
+            end
         end
-      end
 
-      # if it wasn't there, then we just add it to the end and remove the least recently used item
-      unless found
-        @arr.shift
-        @arr << added_ele
-      end
-
+        # if it wasn't there, then we just remove the least recently used item 
+        # and add the new element to the end 
+        unless found
+            @arr.shift if count == @max_capacity
+            @arr << added_ele
+        end
     end
 
     def show
-      # shows the items in the cache, with the LRU item first
-      p @arr
+        # shows the items in the cache, with the LRU item first
+        p @arr
     end
 
     private
-    
+
     # shift all elements after index i to the left by 1 (deleting the item at index i)
-    def shift(index)
-
-        
-
+    def shift_elements(index)
+        (index...@arr.length-1).each do |i|
+            @arr[i] = @arr[i+1]
+        end
     end
+end
 
-  end
+cache = LRUCache.new(4)
+
+cache.add("I walk the line")
+cache.add(5)
+
+p cache.count # => returns 2
+
+cache.add([1,2,3])
+cache.add(5)
+cache.add(-5)
+cache.add({a: 1, b: 2, c: 3})
+cache.add([1,2,3,4])
+cache.add("I walk the line")
+cache.add(:ring_of_fire)
+cache.add("I walk the line")
+cache.add({a: 1, b: 2, c: 3})
+
+
+cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
