4e737f1564083f37cdecd9bae0394930aa6eee53
David <david.espinosa.21@gmail.com>
2021-07-14 16:06:12 -0500

    Make classes for each table

--- questions.rb
+++ questions.rb
@@ -16,15 +16,82 @@ end
 class Question
     attr_accessor :id, :title, :body, :user_id
 
-    def initialize(id,title,body,user_id)
-        @id = id
-        @title = title
-        @body = body
-        @user_id = user_id
-    end
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+      end
+
 
     def self.all
         data = QuestionDBConnection.instance.execute("SELECT * FROM questions")
-        data.map { |id,title,body,user_id | Question.new(id,title,body,user_id) }
+        data.map { |ele| Question.new(ele) }
+    end
+end
+
+class User
+    attr_accessor :id, :fname, :lname
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+      end
+
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM users")
+        data.map { |ele| User.new(ele) }
+    end
+end
+
+class QuestionFollow
+    attr_accessor :id, :user_id, :question_id
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+      end
+
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM questions_follows")
+        data.map { |ele| QuestionFollow.new(ele) }
+    end
+end
+
+class Reply
+    attr_accessor :id, :question_id, :parent_reply_id, :user_id, :body
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @parent_reply_id = options['parent_reply_id']
+        @user_id = options['user_id']
+        @body = options['body']
+      end
+
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM replies")
+        data.map { |ele| Reply.new(ele) }
+    end
+end
+
+class QuestionLike
+    attr_accessor :id, :user_id, :question_id
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+      end
+
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM question_likes")
+        data.map { |ele| QuestionLike.new(ele) }
     end
 end
