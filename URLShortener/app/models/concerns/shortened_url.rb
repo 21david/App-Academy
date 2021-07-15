9906be5e77b56663dd2de5a31cf89682678d8102
David <david.espinosa.21@gmail.com>
2021-07-15 16:17:13 -0500

    phase 2

--- URLShortener/app/models/concerns/shortened_url.rb
+++
@@ -1,5 +0,0 @@
-class ShortenedUrl < ApplicationRecord
-    validates :long_url, presence: true
-    validates :short_url, presence: true, uniqueness: true
-    validates :user_id, presence: true
-end
