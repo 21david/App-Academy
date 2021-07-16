84afb6c84d52c03405c36a0e42972d55d2b2daf6
Quinn Mullaney <skiingqm@gmail.com>
2021-07-16 14:04:07 -0400

    Added harrison ford and it_was_ok methods

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select('id', 'title', 'score').where(score: (2..3))
 end
 
 def harrison_ford
@@ -21,6 +22,7 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
 
+  puts Movie.joins(:actors).where("name = 'Harrison Ford' AND ord != 1").select("movies.id", "title").to_sql
 end
 
 def biggest_cast
