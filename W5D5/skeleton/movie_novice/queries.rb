5505fa8f901c65cc8f93ad7dc9cd27fe4aeff5c2
David <david.espinosa.21@gmail.com>
2021-07-18 13:34:18 -0500

    Finish W5D5 homework (Movie Bluff in Training)

--- W5D5/skeleton/movie_novice/queries.rb
+++ W5D5/skeleton/movie_novice/queries.rb
@@ -24,13 +24,16 @@
 def find_angelina
   #find Angelina Jolie by name in the actors table
   name = 'Angelina Jolie'
-  Actor.find_by_name(name)
+  Actor.find_by(name: name)
+
+  # or
+  # Actor.find_by_name(name)
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie.select(:id, :title).where('score >= 9')
 end
 
 def star_wars
@@ -45,7 +48,7 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  Movie.select('yr', 'COUNT(title) AS bad_movies').where('score < 5').group('yr').order('bad_movies DESC')
+  Movie.select('yr', 'COUNT(id) AS bad_movies').where('score < 5').group('yr').order('yr DESC')
 end
 
 def alphabetized_actors
@@ -54,14 +57,14 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-  Actor.select('name').order('ASC').limit(10)
+  Actor.order('name').limit(10)  # .select('*') is implied if there is no select, it seems
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor.joins(:movies).where(movies: { title: 'Pulp Fiction'})
 end
 
 def uma_movies
@@ -69,5 +72,5 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie.select(:id, :title, :yr).joins(:actors).where(actors: { name: 'Uma Thurman'}).order(:yr)
 end
