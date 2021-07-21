5cb31a100154846f7e9b001a83adbb3ae0df1d2c
David <david.espinosa.21@gmail.com>
2021-07-21 14:43:36 -0500

    finished first project. Sam is working from my computer now

--- FirstRouteController/app/controllers/users_controller.rb
+++ FirstRouteController/app/controllers/users_controller.rb
@@ -1,13 +1,44 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        p params[:id]
+
+        render json: @user
     end
 
     def create
-        render json: params
+        @user = User.new(user_params)
+        
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            render json: @user.error.full_messages, status: 422
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.error.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        @user.destroy
+        redirect_to user_url(@user)
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
