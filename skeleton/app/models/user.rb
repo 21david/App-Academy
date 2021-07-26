a6a2f80e3d15ca77aa9eaa03f4e52361ee14f3d7
anthill499 <anthill499@gmail.com>
2021-07-26 15:45:36 -0400

    finished FBC

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,6 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
 class User < ApplicationRecord
-    validates :user_name, presence: true, uniqueness: { scope: :session_token }
+    validates :user_name, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
+
 
     def reset_session_token!
         self.session_token = SecureRandom.urlsafe_base64(16)
@@ -10,10 +20,29 @@ class User < ApplicationRecord
 
     def password=(pw)
         @password = pw
-        self.password_digest = BCrypt::Password.create(pw)  # can we pass in @password?
+        self.password_digest = BCrypt::Password.create(pw)  
+        # can we pass in @password?
+        # pw -> string class
+        # bcrypt::password class
     end
 
     def is_password?(pw)
-        BCrypt::Password.new(self.password_digest).is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw) # built-in ruby method
+        # BCrypt::Password.new(self.password_digest) == pw            # not 'fool-proof'
+
+        # what is this doing?
+        # hashed pw -> BCrypt::Password class
     end
+
+    def self.find_by_credentials(un, pw)
+        @user = User.find_by(user_name: un)           # return an instance of user class
+        # BCrypt == and String == may be doing different things under the hood.
+        return 'User doesnt exist' if @user.nil?
+        @user.is_password?(pw) ? (return @user) : (p "password does not match #{un}'s password") # checking similarity
+    end
+
+
+
+    
+
 end
