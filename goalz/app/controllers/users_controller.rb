dc31dca8429e59c37b492bf8ae4f90f5e29570e1
Aiden Chang <skylineciel@gmail.com>
2021-07-28 15:28:48 -0400

    working on the integration test

--- goalz/app/controllers/users_controller.rb
+++ goalz/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_session_url
+        else
+            flash[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
