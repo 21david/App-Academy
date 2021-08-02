78b68ccbd418ed8bc49b138ddb8418ac6439ca7d
David <david.espinosa.21@gmail.com>
2021-08-02 13:54:13 -0500

    working on controller_base

--- rails_lite_part_1_skeleton/skeleton/lib/controller_base.rb
+++ rails_lite_part_1_skeleton/skeleton/lib/controller_base.rb
@@ -8,20 +8,37 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise "Cannot render twice"
+    else
+      @res.status = 302
+      @res["Location"] = url
+      @already_built_response = true
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
-  def render_content(content, content_type)
+  def render_content(content, content_type = "text/html")
+    if already_built_response?
+      raise "Cannot render twice"
+    else
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      @already_built_response = true
+    end
   end
 
   # use ERB and binding to evaluate templates
