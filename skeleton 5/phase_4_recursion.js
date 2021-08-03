5fd0427ffe88b009fdede0e75de1cd4c7016c239
David <david.espinosa.21@gmail.com>
2021-08-03 15:59:05 -0500

    working on merge sort

--- skeleton 5/phase_4_recursion.js
+++ skeleton 5/phase_4_recursion.js
@@ -72,10 +72,11 @@ function deepDup(arr) {
 
 
 function bsearch(arr,target){
+    // debugger
     if (arr.length===0){
         return -1;
     }
-    const mid = arr.length/2;
+    const mid = parseInt(arr.length/2);
     if(arr[mid]===target){
         return mid;
     }
@@ -91,5 +92,43 @@ function bsearch(arr,target){
     }
 }
 
-arr=[1,2,3,4,5,6,7,8];
-console.log(bsearch(arr,5));
+// arr=[1,2,3,4,5,6,7,8];
+// console.log(bsearch(arr,0));
+// console.log(bsearch(arr,2));
+// console.log(bsearch(arr,6));
+// console.log(bsearch(arr,6.5));
+// console.log(bsearch(arr,7));
+// console.log(bsearch(arr,8));
+// console.log(bsearch(arr,32));
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const left = arr.slice(0, parseInt(arr.length/2))
+    const right = arr.slice(parseInt(arr.length/2))
+
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return merge(sortedLeft, sortedRight);
+}
+
+function merge(arr1, arr2) {
+    const merged = [];
+    while(arr1.length !== 0 && arr2.length !== 0) {
+        if (arr1[0] < arr2[0]) {
+            merged.push(arr1.shift)
+        }
+        else {
+            merged.push(arr2.shift)
+        }
+    }
+
+    return merged;
+}
+
+
+const array4 = [81, 49, 30, 55, 13, 9, 21, -4, 33, 24];
+console.log(mergeSort(array4));
