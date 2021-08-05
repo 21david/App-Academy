b7277aa04183ee846e08684198088bfaaa93ba64
cgradeff <64032507+cgradeff@users.noreply.github.com>
2021-08-05 16:44:39 -0400

    swap

--- intro_to_callbacks/absurdBubbleSort.js
+++ intro_to_callbacks/absurdBubbleSort.js
@@ -9,15 +9,12 @@ const reader = readline.createInterface({
 function askIfGreaterThan(el1, el2, callback) {
   // Prompt user to tell us whether el1 > el2; pass true back to the
   // callback if true; else false.
-  reader.question(`Is ${el1} greater than ${el2}?`, response => {
-      if(el1 > el2 && response === "yes") {
-        callback(true)
-      }
-      else if(el1 <= el2 && response === "no") {
-        callback(true)
+  reader.question(`Is ${el1} greater than ${el2}? `, response => {
+      if(response === "yes") {
+        return callback(true);
       }
       else {
-        callback(false)
+        return callback(false);
       }
   })
 }
@@ -27,14 +24,19 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
   // Do an "async loop":
   // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
   //    know whether any swap was made.
-  if (i == arr.length - 1) {
-    outerBubbleSortLoop(madeAnySwaps)
+  if (i === arr.length - 1) {
+    outerBubbleSortLoop(madeAnySwaps);
   }
   else {
-    let swap = askIfGreaterThan(arr[i], arr[i+1], (x) => {return x})
-    if(swap) {
-      [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-    }
+    askIfGreaterThan(arr[i], arr[i+1], (x) => {
+        if(x) {
+            [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+            madeAnySwaps = true;
+        } 
+        innerBubbleSortLoop(arr, (i + 1), madeAnySwaps, outerBubbleSortLoop);
+    });
+    
+    
   }
   // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
   //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
