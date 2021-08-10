772f7e176b481937b1204d247b2275219c517d6d
OussamaElar <oussama.elar@gmail.com>
2021-08-10 14:15:29 -0400

    ttt

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -3,4 +3,7 @@ const Game = require('../ttt_node/game'); // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const htmlElement = document.querySelector('.ttt')
+  const view = new View(game, htmlElement);
 });
--- tic_tac_toe/src/ttt-view.js
+++ tic_tac_toe/src/ttt-view.js
@@ -4,7 +4,14 @@ class View {
     this.el = el;
   }
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement('ul');
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        ul.append(document.createElement('li'));
+      }
+    }
+  }
   
   bindEvents() {}
 
