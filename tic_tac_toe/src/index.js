81f352e44c1f15426b9dfc793b724a9a00a5cf88
David <david.espinosa.21@gmail.com>
2021-08-10 16:25:16 -0500

    working on mechanism for ending the game

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -4,6 +4,6 @@ const Game = require('../ttt_node/game'); // require appropriate file
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const game = new Game();
-  const htmlElement = document.querySelector('.ttt')
+  const htmlElement = document.querySelector('.ttt');
   const view = new View(game, htmlElement);
 });
--- tic_tac_toe/src/ttt-view.js
+++ tic_tac_toe/src/ttt-view.js
@@ -34,11 +34,26 @@ class View {
     // const symbol = document.createElement('p')
     // symbol.innerText = this.game.currentPlayer;
     // square.appendChild(symbol)
-    square.classList.toggle(this.game.currentPlayer)
+    
     let row = square.dataset.row;
     let column = square.dataset.column;
     let pos = [parseInt(row), parseInt(column)];
-    this.game.playMove(pos);
+    // this.game.playMove(pos);
+    try { 
+      square.classList.toggle(this.game.currentPlayer);
+      this.game.playMove(pos);
+    }
+    catch (e) {
+      alert('This is an invalid move!');
+    }    
+
+    if(this.game.isOver()) {
+      if (this.game.winner()) {
+        alert(`${this.game.winner().toUpperCase()} has won!`);
+      } else {
+        alert('NO ONE WINS!');
+      }
+    }
   }
 
 }
