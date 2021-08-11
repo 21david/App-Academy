3bf6d492ff1280d7c201141ff9797310e9c61f18
David <david.espinosa.21@gmail.com>
2021-08-11 14:32:03 -0500

    working on FollowToggle#handleClick

--- skeleton/frontend/follow_toggle.js
+++ skeleton/frontend/follow_toggle.js
@@ -1,7 +1,28 @@
-function FollowToggle($el) {
-    this.userId = $el.find("data-user-id");
-    this.followState = $el.find("data-initial-follow-state");
+function FollowToggle($el) { // $el is the button
+    // this.userId = $el.find("data-user-id");
+    // this.followState = $el.find("data-initial-follow-state");
+    this.userId = $el.data('user-id');
+    this.followState = $el.data('initial-follow-state');
     this.el = $el;
+    this.el.innerText = this.render();
+}
+
+FollowToggle.prototype.render = function() {
+    this.followState === 'followed' ? "Follow!" : "Unfollow!"
+}
+
+FollowToggle.prototype.handleClick = function(e) {
+    e.preventDefault();
+    if(this.render() === 'Unfollow!') {
+        $.ajax({
+            method: "POST",
+            url: '/users/'
+        });
+    } else {
+        $.ajax({
+
+        });       
+    }
 }
 
 module.exports = FollowToggle;
