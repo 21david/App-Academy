303b12465d065069f7a4326a6a97ac60240b8ae3
David <david.espinosa.21@gmail.com>
2021-08-23 10:49:16 -0500

    working on phase 2

--- minesweeper/components/Board.jsx
+++ minesweeper/components/Board.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 class Board extends React.Component {
     constructor (props) {
         super(props);
+        this.grid = [];
         
+        for(let i = 0; i < 9; i++) {
+            let arr = [];
+            arr[8] = undefined;
+            this.grid.push(arr);
+        }
     }
 
     render () {
         return (
             <div>
-                {this.props.board.map((row, index)=> {
+                {this.props.board.grid.map((row, index)=> {
                     return <div key={`${index} + ${row}`}>{row.map((tile, tileIndex) => {
                         return <Tile key={`${tileIndex} + ${tile}`} tile={tile}  update={this.props.update}/>
                     })}</div>
